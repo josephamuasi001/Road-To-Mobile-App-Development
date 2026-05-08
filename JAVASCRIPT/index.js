a7f3327fac5c522e414db443c35e40fc4044b1fd
josephamuasi001 <jamuasi001@st.ug.edu.gh>
2026-05-08 11:02:12 +0000

    My first js file

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,7 @@
+// console.log('Hello')
+// console.log('I like pizza')
+
+// window.alert('This is an alert')
+// window.alert("I like pizza")
+
+document.getElementById("myp").textContent = 'Hello';
